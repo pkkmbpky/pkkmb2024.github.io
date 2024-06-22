a15b5b317508352eea5a78ce3a8fd3556c3cd096
pkkmb2024 <pkkmblegislatif2024@gmail.com>
2024-06-22 21:04:52 +0700

    menambahkan cp

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function showWhatsAppAlert() {
-  const whatsappLink1 = 'http://wa.me/62882006385928';
-  const whatsappLink2 = 'http://wa.me/6283878666225';
+  const whatsappLink1 = 'http://wa.me/6282198166883';
+  const whatsappLink2 = 'http://wa.me/6281390932149';
+  const whatsappLink3 = 'http://wa.me/628137075361';
 
   // Membuat elemen div untuk menampilkan alert
   const alertDiv = document.createElement('div');
@@ -13,12 +14,14 @@ function showWhatsAppAlert() {
   alertDiv.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
   alertDiv.style.border = '1px solid #ccc';
   alertDiv.style.zIndex = '1000';
+  alertDiv.style.borderRadius = '15px';
 
   // Menambahkan pesan ke dalam div
   alertDiv.innerHTML = `
         <p>Klik link berikut untuk membuka WhatsApp:</p>
-        <p><a href="${whatsappLink1}" target="_blank">${whatsappLink1}</a></p>
-        <p><a href="${whatsappLink2}" target="_blank">${whatsappLink2}</a></p>
+        <p>Kezia : <a href="${whatsappLink1}" target="_blank">${whatsappLink1}</a></p>
+        <p>Diah : <a href="${whatsappLink2}" target="_blank">${whatsappLink2}</a></p>
+        <p>Alvin : <a href="${whatsappLink3}" target="_blank">${whatsappLink3}</a></p>
         <button onclick="closeAlert()">Tutup</button>
     `;
 
